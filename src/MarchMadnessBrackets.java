5031877711b00c4e349fe0e486ec0253599e0a93
Raymon Atienza <raymon.atienza@gmail.com>
2014-04-10 15:33:59 -0400

    Added arguments into one string

---
+++ src/MarchMadnessBrackets.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Raymon on 2014-04-10.
+ */
+import java.util.ArrayList;
+
+public class MarchMadnessBrackets {
+
+    public static void main(String[] args) {
+
+        String temp="";
+
+        for (int i=0; i < args.length; i++) {
+            temp += args[i] + " ";
+        }
+
+        System.out.println("Input String: " + temp);
+
+
+
+
+
+    }
+}
